0a40918482ef07e32ead51ebd9b2f6202f04d9c4
Akbib9 <11740@etu.he2b.be>
2022-11-01 15:15:21 +0100

    A* V1 fonctionnel

--- Partie1/Astar.py
+++ Partie1/Astar.py
@@ -56,11 +56,11 @@ graph = {
 thesee = (6,1)
 minotaure = (2,5)
 
-def dfs(goal, start, graph):
+def a_star(goal, start, graph):
     queue = [[start]]
-    print(queue)
+    print("La file est initiée au noeud start :", queue)
     node1 =[0,0]
-    while len(queue) != 0:
+    while len(queue) != 0 :
         node = list(queue[0][-1]) # coordonnées en mode liste ex : (2.5) -> [2.5]
         infoNode = graph[queue[0][-1]].copy() # dico avec infos sur direction {'W':1, 'N':0, 'E':0, 'S':0}
         save = queue.pop(0)
@@ -80,18 +80,71 @@ def dfs(goal, start, graph):
                     node1[1] = node[1] - 1
                     node1[0] = node[0]
                 kidpath = save.copy()
-                node2 = tuple(node1)
+                node2 = node1.copy()
+                node2 = tuple(node2)
                 if node2 not in kidpath:
                     kidpath.append(node2)
                     queue.insert(0,kidpath)
                     print("File incrémentée d'un noeud enfant ", queue)
                 if node2 == goal:
-                    print("Dernière queue : ",queue)
-                    return kidpath
+                    print("Ce chemin à atteint le goal", node2)
+        queue = sorting(queue) #Tri de la queue entière après insertion des noeuds enfants
+        print("Etat de la file après le tri : ", queue, len(queue))
+        boole = False
+        while boole == False : #Gesion des chemins redondants
+            boole, queue = delete_rundondant_paths(queue, graph, boole)
+        if goal in queue[0]:
+            print("Dernière queue : ",queue, len(queue))
+            return queue[0]
         infoNode = infoNode.clear()
         print("Nouvelle queue : ", queue)
 
-a = dfs(thesee, minotaure, graph)
-print("Le chemin enfant est : ",a)
 
-#Version1
+def heuristic(graph, goal):
+    for cle in graph.keys():
+        xgoal = goal[0]
+        ygoal = goal[1]
+        #print(xgoal, ygoal)
+        xcle = cle[0]
+        ycle = cle[1]
+        #print(xcle, ycle)
+        diffx = abs(xgoal-xcle)
+        diffy = abs(ygoal-ycle)
+        #print(diffx, diffy)
+        heurinode = diffx+diffy
+        #print(heurinode)
+        graph[cle]['H'] = heurinode
+        #print(graph[cle])
+
+def accumulated_cost(path):
+    cost = len(path)-1
+    return cost
+
+def sum_heuristic_accumulated_cost(path, graph):
+    sum = accumulated_cost(path) + graph[path[-1]]['H']
+    #print(" noeud ", path[-1], " cout accumulé + heuristic: ", accumulated_cost(path), "+ ", graph[path[-1]]['H'], "= ", sum)
+    return sum
+
+def delete_rundondant_paths(path, graph, boole):
+    for i in range (len(path)):
+        for j in range(len(path)):
+            #print("dernier noeud du chemin : ", path[i][-1], path[j])
+            if i != j and path[i][-1] in path[j] and sum_heuristic_accumulated_cost(path[i], graph) >= sum_heuristic_accumulated_cost(path[j], graph):
+                del(path[i])
+                boole = False
+                return boole, path
+    boole = True
+    return boole, path
+                
+def sorting(liste):
+    for i in range(len (liste)):
+        mini=i
+        for j in range(i+1, len (liste)):
+            if sum_heuristic_accumulated_cost(liste[j], graph) < sum_heuristic_accumulated_cost(liste[mini], graph) :
+                #print(sum_heuristic_accumulated_cost(liste[j], graph), sum_heuristic_accumulated_cost(liste[mini], graph))
+                mini = j
+        liste[i], liste[mini] = liste[mini], liste[i]
+    return liste
+
+heuristic(graph, thesee)
+print('le chemin choisi est : ', a_star(thesee, minotaure, graph))
