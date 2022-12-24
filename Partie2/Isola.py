aac42544ba63cb2de1f7bfb26f5c749bd050bbd6
Akbib9 <11740@etu.he2b.be>
2022-12-24 18:04:41 +0100

    Isola human Vs Human (part2)
    On peut jouer en 1 contre 1

--- Partie2/Isola.py
+++ Partie2/Isola.py
@@ -23,38 +23,46 @@ def get_adjacent_positions(pos,game_board):
         if j > 0:  # Case en haut à gauche
             if game_board[i-1][j-1] != "X":
                 adjacent_positions.append((i-1, j-1))
-        if j < 7:  # Case en haut à droite
+        if j < 5:  # Case en haut à droite
             if game_board[i-1][j+1] != "X":
                 adjacent_positions.append((i-1, j+1))
-    if i < 7:  # Case en-dessous
+    if i < 5:  # Case en-dessous
         if game_board[i+1][j] != "X":
             adjacent_positions.append((i+1, j))
         if j > 0:  # Case en bas à gauche
             if game_board[i+1][j-1] != "X":
                 adjacent_positions.append((i+1, j-1))
-        if j < 7:  # Case en bas à droite
+        if j < 5:  # Case en bas à droite
             if game_board[i+1][j+1] != "X":
                 adjacent_positions.append((i+1, j+1))
     if j > 0:  # Case à gauche
         if game_board[i][j-1] != "X":
             adjacent_positions.append((i, j-1))
-    if j < 7:  # Case à droite
+    if j < 5:  # Case à droite
         if game_board[i][j+1] != "X":
             adjacent_positions.append((i, j+1))
-
+    if len(adjacent_positions) == 0:
+        return 0
     return adjacent_positions
 
 ########################################
 
 'Change Position of Player'
 
-def PlayerMove(player_pos, adv_pos):
+def PlayerMove(player_pos, adv_pos,nbr_player):
     while True:
     # Choisir une case ou avancer
+        if get_adjacent_positions(player_pos,game_board) == 0:
+            return 0
         print("Positions valides :", get_adjacent_positions(player_pos,game_board))
+        game_board[player_pos[0]][player_pos[1]] = " "
         i, j = map(int, input("Entrez les coordonnées de la case (ligne colonne) : ").split())
         if (i, j) in get_adjacent_positions(player_pos,game_board) and (i, j) != adv_pos:
             player_pos = (i, j)
+            if nbr_player == 1:
+                game_board[player_pos[0]][player_pos[1]] = "1"
+            else : 
+                game_board[player_pos[0]][player_pos[1]] = "2"
             return player_pos
         else:
             print("Case non valide, veuillez réessayer")
@@ -83,30 +91,38 @@ def BlokCase(game_board, player_pos, adv_pos):
 
 # Création du tableau de jeu
 game_board = [
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "],
-    [" "," "," "," "," "," "," "," "]
+    [" "," "," "," "," "," "],
+    [" "," "," "," "," "," "],
+    [" "," "," "," "," "," "],
+    [" "," "," "," "," "," "],
+    [" "," "," "," "," "," "],
+    [" "," "," "," "," "," "],
 ]
 
 # Positions initiales des pions
 player1_pos = (0,4)  # Coin supérieur gauche
-player2_pos = (7,3)  # Coin inférieur droit
-
-adjacent_positions = get_adjacent_positions(player1_pos,game_board)
-print(adjacent_positions)  # [(1,0), (0,1), (1,1)]
+player2_pos = (5,3)  # Coin inférieur droit
+game_board[player1_pos[0]][player1_pos[1]] = "1"
+game_board[player2_pos[0]][player2_pos[1]] = "2"
 
 while True:  # Boucle de jeu
+
     # Tour du joueur 1
     print("Au tour du joueur 1")
-    player1_pos = PlayerMove(player1_pos,player2_pos)
+    print(np.array(game_board))
+    nbr_player = 1
+    player1_pos = PlayerMove(player1_pos,player2_pos,nbr_player)
+    if player1_pos == 0:
+        print("Joueur 1, vous avez perdu, il n'y a plus moyen de bouger votre pion")
+        break
     BlokCase(game_board, player1_pos, player2_pos)
+
     # Tour du joueur 2
     print("Au tour du joueur 2")
-    player2_pos = PlayerMove(player2_pos,player1_pos)
-    BlokCase(game_board, player2_pos, player1_pos)
     print(np.array(game_board))
+    nbr_player = 2
+    player2_pos = PlayerMove(player2_pos,player1_pos,nbr_player)
+    if player2_pos == 0:
+        print("Joueur 2, vous avez perdu, il n'y a plus moyen de bouger votre pion")
+        break
+    BlokCase(game_board, player2_pos, player1_pos)
